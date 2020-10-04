54f64b046fa713b5547058aa5244ffd10ec857af
sharadshrivastava <shrivastava.lucky@gmail.com>
2020-10-04 20:38:02 +1100

    Initial Commit.

--- app/src/main/java/com/test/app/ui/employees/EmployeesListViewModel.kt
+++ app/src/main/java/com/test/app/ui/employees/EmployeesListViewModel.kt
@@ -8,13 +8,13 @@ import com.test.app.data.network.Resource
 import com.test.app.domain.model.Item
 import com.test.app.domain.usecases.NewsUseCase
 
-class NewsViewModel @ViewModelInject constructor(
+class EmployeesListViewModel @ViewModelInject constructor(
     private val useCase: NewsUseCase,
     @Assisted private val savedStateHandle: SavedStateHandle
 ) : ViewModel() {
 
     var clickListener  = MutableLiveData<Item>()
-    var layoutIds = arrayOf(R.layout.layout_news_item_latest, R.layout.layout_news_item)
+    var layoutIds = arrayOf(R.layout.layout_employee_item)
 
     var feeds = liveData {
         emit(Resource.loading())
