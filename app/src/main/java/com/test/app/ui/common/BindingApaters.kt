06f53f80c011023064f0e48430414c309d6dd73b
sharadshrivastava <shrivastava.lucky@gmail.com>
2020-10-06 22:46:28 +1100

    Added placeholder image.

--- app/src/main/java/com/test/app/ui/common/BindingApaters.kt
+++ app/src/main/java/com/test/app/ui/common/BindingApaters.kt
@@ -4,9 +4,11 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.test.app.R
 
 @BindingAdapter("downloadUrl")
-fun loadImage(imageView: ImageView, url: String?) = Glide.with(imageView).load(url).into(imageView)
+fun loadImage(imageView: ImageView, url: String?) =
+    Glide.with(imageView).load(url).placeholder(R.drawable.user).into(imageView)
 
 @BindingAdapter(value = ["items", "itemLayout", "clickListener"], requireAll = true)
 fun <T> configureRecyclerView(
