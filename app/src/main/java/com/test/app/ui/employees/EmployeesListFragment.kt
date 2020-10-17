c31368c9535a3b1cae303115682d0f5317b8cef2
sharadshrivastava <shrivastava.lucky@gmail.com>
2020-10-18 07:37:23 +1100

    updated Gradle and Kotlin.

--- app/src/main/java/com/test/app/ui/employees/EmployeesListFragment.kt
+++ app/src/main/java/com/test/app/ui/employees/EmployeesListFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.DividerItemDecoration
 import com.test.app.R
 import com.test.app.data.network.Resource.Status.*
