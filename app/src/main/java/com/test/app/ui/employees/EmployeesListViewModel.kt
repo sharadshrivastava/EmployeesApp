94ad95e989a60bf26cd33f222b42e75dc13b3851
sharadshrivastava <shrivastava.lucky@gmail.com>
2020-10-18 08:22:10 +1100

    added refresh functionality.

--- app/src/main/java/com/test/app/ui/employees/EmployeesListViewModel.kt
+++ app/src/main/java/com/test/app/ui/employees/EmployeesListViewModel.kt
@@ -4,19 +4,28 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
 import com.test.app.data.network.Resource
-import com.test.app.domain.model.Employee
 import com.test.app.domain.usecases.EmployeesUseCase
-import com.test.app.ui.common.ItemClickListener
 
 class EmployeesListViewModel @ViewModelInject constructor(
     private val useCase: EmployeesUseCase
 ) : ViewModel() {
 
-    var clickListener = MutableLiveData<Employee>()
+    private var emitLoading = true
+    private val loadTrigger = MutableLiveData(Unit)
 
-    var employees = liveData {
-        emit(Resource.loading())
+    var employees = loadTrigger.switchMap {
+        fetchEmployees()
+    }
+
+    fun refresh() {
+        emitLoading = false
+        loadTrigger.value = Unit
+    }
+
+    private fun fetchEmployees() = liveData {
+        if (emitLoading) emit(Resource.loading())
         emit(useCase.employees())
     }
 }
