2ec4526967321419b67a5e6f93a6e160f505f80d
sharadshrivastava <shrivastava.lucky@gmail.com>
2021-05-04 19:15:35 +1000

    Added unique constraint in DB.

--- app/src/main/java/com/test/app/data/db/entity/Employee.kt
+++ app/src/main/java/com/test/app/data/db/entity/Employee.kt
@@ -3,14 +3,15 @@ package com.test.app.data.db.entity
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-@Entity
+@Entity(indices = [Index(value = ["full_name", "team"], unique = true)])
 data class Employee(
     @ColumnInfo(name = "full_name") val fullName: String?,
     @ColumnInfo(name = "photo_url_small") val imgUrl: String?,
     @ColumnInfo(name = "team") val team: String?,
     @PrimaryKey(autoGenerate = true) val id: Int = 0
-):Parcelable
+) : Parcelable
