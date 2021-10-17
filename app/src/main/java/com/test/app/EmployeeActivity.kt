f53f05606c2ce7e0f99b24a8b5d71a571922feb9
sharadshrivastava <shrivastava.lucky@gmail.com>
2021-10-17 12:11:36 +1100

    optimized for NavController.

--- app/src/main/java/com/test/app/EmployeeActivity.kt
+++ app/src/main/java/com/test/app/EmployeeActivity.kt
@@ -3,8 +3,8 @@ package com.test.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,14 +16,8 @@ class EmployeeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
-        navController = navHostFragment.navController
-    }
-
-    override fun onStart() {
-        super.onStart()
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        navController = findNavController(R.id.navHostFragment)
+        setupActionBarWithNavController(this, navController)
     }
 
     override fun onSupportNavigateUp() = navController.navigateUp()
